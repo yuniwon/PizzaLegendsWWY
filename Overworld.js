f8b57b77367e1251188ebfb9e6c119fc162c1d24
yuniwon <dnjsdndus@gmail.com>
2024-01-23 23:11:45 +0900

    create: 게임 오브젝트 추가

--- Overworld.js
+++ Overworld.js
@@ -1,55 +1,72 @@
 class Overworld {
- constructor(config) {
-   this.element = config.element;
-   this.canvas = this.element.querySelector(".game-canvas");
-   this.ctx = this.canvas.getContext("2d");
- }
+  constructor(config) {
+    this.element = config.element;
+    this.canvas = this.element.querySelector(".game-canvas");
+    this.ctx = this.canvas.getContext("2d");
+  }
 
- init() {
-   const image = new Image();
-   image.onload = () => {
-     this.ctx.drawImage(image,0,0)
-   };
-   image.src = "/images/maps/DemoLower.png";
+  init() { //this is the first thing that run
+    const image = new Image();
+    image.onload = () => {
+      this.ctx.drawImage(image, 0, 0)
+    };
+    image.src = "/images/maps/DemoLower.png";
+
+    const hero = new GameObject({
+      x: 5,
+      y: 6,
+    });
+    const npc1 = new GameObject({
+      x: 7,
+      y: 9,
+      src: "/images/characters/people/npc1.png",
+    });
+
+    setTimeout(() => {
+      hero.sprite.draw(this.ctx);
+      npc1.sprite.draw(this.ctx);
+    }, 200);
+    // hero.sprite.draw(this.ctx);
+    // npc1.sprite.draw(this.ctx);
+    // animate();
+    // const x = 5;
+    // const y = 6;
+
+    // const shadow = new Image();
+    // shadow.onload = () => {
+    //   this.ctx.drawImage(
+    //     shadow,
+    //     0, //left cut 
+    //     0, //top cut,
+    //     32, //width of cut
+    //     32, //height of cut
+    //     x * 16 - 8, //x position
+    //     y * 16 - 18, //y position
+    //     32, //width of image
+    //     32 //height of image
+    //   )
+    // }
+    // shadow.src = "/images/characters/shadow.png";
 
 
-   const x = 5;
-   const y = 6;
+    // const hero = new Image();
+    // hero.onload = () => {
+    //   this.ctx.drawImage(
+    //     hero,
+    //     0, //left cut 
+    //     0, //top cut,
+    //     32, //width of cut
+    //     32, //height of cut
+    //     x * 16 - 8,
+    //     y * 16 - 18,
+    //     32,
+    //     32
+    //   )
+    // }
+    // hero.src = "/images/characters/people/hero.png";
 
-   const shadow = new Image();
-   shadow.onload = () => {
-    this.ctx.drawImage(
-      shadow, 
-      0, //left cut 
-      0, //top cut,
-      32, //width of cut
-      32, //height of cut
-      x * 16 - 8,
-      y * 16 - 18,
-      32,
-      32
-   )
-   }
-   shadow.src = "/images/characters/shadow.png";
-
-
-   const hero = new Image();
-   hero.onload = () => {
-     this.ctx.drawImage(
-       hero, 
-       0, //left cut 
-       0, //top cut,
-       32, //width of cut
-       32, //height of cut
-       x * 16 - 8,
-       y * 16 - 18,
-       32,
-       32
-    )
-   }
-   hero.src = "/images/characters/people/hero.png";
+  }
 
 
- }
 
 }
---
+++ Sprite.js
@@ -0,0 +1,51 @@
+class Sprite {
+  constructor(config) {
+
+    // 이미지 셋업
+    this.image = new Image();
+    this.image.src = config.src;
+    this.image.onload = () => {
+      this.isLoaded = true;
+    };
+
+    // 그림자
+    this.shadow = new Image();
+    this.useShadow = true;
+    if (this.useShadow) {
+      this.shadow.src = "/images/characters/shadow.png";
+    }
+    this.shadow.onload = () => {
+      this.isShadowLoaded = true;
+    };
+
+    //애니메이션 설정 & 초기 상태
+    this.animations = config.animations || {
+      idleDown: [
+        [0, 0]
+      ],
+
+    };
+    this.currentAnimation = config.currentAnimation || "idleDown";
+    this.currentFrame = config.currentFrame || 0;
+
+    // 게임 오브젝트 레퍼런스
+    this.gameObject = config.gameObject || null;
+  }
+
+  draw(ctx) {
+    const x = this.gameObject.x * 16 - 8;
+    const y = this.gameObject.y * 16 - 18;
+    this.isShadowLoaded && ctx.drawImage(
+      this.shadow,
+      x, //x position
+      y, //y position
+    );
+    this.isLoaded && ctx.drawImage(this.image,
+      0, 0,
+      32, 32,
+      x, y,
+      32, 32
+    );
+  }
+
+}
